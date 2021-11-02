04ec353f4d9054ac39daf5dc44d066dc8b9dbad1
KarenStewart1 <karenstewart1117@gmail.com>
2021-11-02 18:10:28 +0100

    created search form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import SearchForm from "./SearchForm";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <div className="container">
+        <h1>Dictionary App</h1>
+        <SearchForm />
+        <footer>Coded by Karen Stewart</footer>
+      </div>
     </div>
   );
 }
