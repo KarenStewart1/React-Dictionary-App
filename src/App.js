96136e45eceb7d264c21afb329feb32661c08d95
KarenStewart1 <karenstewart1117@gmail.com>
2021-11-03 11:38:36 +0100

    added link to github

--- src/App.js
+++ src/App.js
@@ -7,7 +7,16 @@ function App() {
       <div className="container">
         <h1>Dictionary App</h1>
         <Dictionary />
-        <footer className="mt-5">Coded by Karen Stewart</footer>
+        <footer className="mt-5">
+          <a
+            href="https://github.com/KarenStewart1/React-Dictionary-App"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Karen Stewart
+        </footer>
       </div>
     </div>
   );
