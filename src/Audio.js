877692a7531f20462e2418c940cd2125396357b0
KarenStewart1 <karenstewart1117@gmail.com>
2021-11-04 11:31:55 +0100

    button fix

--- src/Audio.js
+++ src/Audio.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Audio(props) {
   return (
-    <button>
-      <a href={props.audio} target="_blank" rel="noreferrer">
+    <a href={props.audio} target="_blank" rel="noreferrer">
+      <button>
         <i className="far fa-play-circle"></i>
-      </a>
-    </button>
+      </button>
+    </a>
   );
 }
